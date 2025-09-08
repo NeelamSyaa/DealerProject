d7357e1c93e3080f665f175d4da21aea35aa7ff6
NeelamSyaa <neelam.P@syaa.in>
2025-09-08 17:17:56 +0530

    deshboad page validation

---
+++ src/test/java/com/dealer/testcases/DeshBoard.java
@@ -0,0 +1,12 @@
+package com.dealer.testcases;
+
+import org.openqa.selenium.By;
+
+public class DeshBoard {
+
+	public void Dashboard() {
+		
+		
+		
+	}
+}
--- src/test/java/com/dealer/testcases/LoginTest.java
+++ src/test/java/com/dealer/testcases/LoginTest.java
@@ -2,13 +2,15 @@ package com.dealer.testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
 	@Test
-	public  void openbrowser() {
+	public  void openbrowser() throws InterruptedException {
 		
 	WebDriver driver = new  ChromeDriver();
 	driver.get("https://pro.smlsaarthi.com/login?redirect=%2F");
@@ -17,6 +19,16 @@ public class LoginTest {
 	driver.findElement(By.xpath("//input[@placeholder=\"Password\"]")).sendKeys("Smlhr@123");
 	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[1]/div/div[2]/div[3]/form/div/div[3]/span/input")).click();
 	driver.findElement(By.xpath("//button[text()=\"Sign In\"]")).click();
+	Thread.sleep(3000);
+	WebElement homepage = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/div[1]/h4[1]"));
+    String actualResult = homepage.getText();
+
+    // Expected result (whatever text appears on dashboard)
+    String expectedResult = "Dashboard";
+
+    // Validation
+    Assert.assertEquals(actualResult, expectedResult, "Login validation failed!");
+	driver.close();
 	
 	}
 }
