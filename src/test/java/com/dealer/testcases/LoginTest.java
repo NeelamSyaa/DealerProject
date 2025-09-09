3e003d5c50eed5ebcf935915e7874784ad5959e0
NeelamSyaa <neelam.P@syaa.in>
2025-09-09 17:59:26 +0530

    pom

--- src/test/java/com/dealer/testcases/LoginTest.java
+++ src/test/java/com/dealer/testcases/LoginTest.java
@@ -14,6 +14,7 @@ public class LoginTest extends BaseTest {
 	@Test
 	public  void openbrowser() throws InterruptedException {
 	driver.findElement(By.id("outlined-size-small")).sendKeys("SMLHR");
+	log.info("user enter the username");
 	driver.findElement(By.xpath("//input[@placeholder=\"Password\"]")).sendKeys("Smlhr@123");
 	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[1]/div/div[2]/div[3]/form/div/div[3]/span/input")).click();
 	driver.findElement(By.xpath("//button[text()=\"Sign In\"]")).click();
